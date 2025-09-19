5377fb3ed79f3d8df4970a22e4adebae7faee6ee
KMHenn <kaithennessy@gmail.com>
2025-09-18 21:54:19 -0400

    basic filtering

--- app/Livewire/CardSearch.php
+++ app/Livewire/CardSearch.php
@@ -10,14 +10,28 @@ class CardSearch extends Component
     public array $featureOptions;
     public array $typeOptions;
     public array $rarityOptions;
+    public array $levelOptions;
+    public array $roundOptions;
+    public array $characterOptionsByGroup;
 
-    public string $searchName;
+    public ?string $searchText = null;
+    public ?string $featureFilter = null;
+    public ?string $typeFilter = null;
+    public ?string $rarityFilter = null;
+    public ?string $levelFilter = null;
+    public ?string $roundFilter = null;
+    public ?string $characterFilter = null;
+
     public $cardResults;
+    public string $activeFilters = '';
 
     public function mount(){
         $this->featureOptions = Card::distinct('feature')->pluck('feature')->toArray();
-        $this->typeOptions = Card::distinct('type')->orderBy('type')->pluck('type')->toArray();
+        $this->typeOptions = Card::distinct('type')->whereNotNull('type')->orderBy('type')->pluck('type')->toArray();
         $this->rarityOptions = Card::distinct('rarity')->orderBy('rarity')->pluck('rarity')->toArray();
+        $this->levelOptions = Card::distinct('level')->whereNotNull('level')->orderBy('level')->pluck('level')->toArray();
+        $this->roundOptions = Card::distinct('round')->whereNotNull('round')->orderBy('round')->pluck('round')->toArray();
+        $this->characterOptionsByGroup = Card::distinct('character_name')->where('character_name', '!=', '-')->orderBy('character_name')->select('feature', 'character_name')->get()->groupBy('feature')->toArray();
     }
 
     public function render()
@@ -25,7 +39,49 @@ class CardSearch extends Component
         return view('livewire.card-search');
     }
 
-    public function updatedSearchName(string $searchText){
-        // $this->cardResults = Card::
+    public function updated(string $field, string $value){
+        if(str_contains($field, 'Filter')){
+            // Filter update
+        }
+
+        $this->filterCards();
+    }
+
+    private function filterCards(){
+        $query = Card::query();
+        $filters = [];
+
+        if(!is_null($this->featureFilter)){
+            $filters[] = 'Feature: ' . $this->featureFilter;
+            $query->where('feature', $this->featureFilter);
+        }
+
+        if(!is_null($this->typeFilter)){
+            $filters[] = 'Type: ' . $this->typeFilter;
+            $query->where('type', $this->typeFilter);
+        }
+
+        if(!is_null($this->rarityFilter)){
+            $query->where('rarity', $this->rarityFilter);
+        }
+
+        if(!is_null($this->levelFilter)){
+            $query->where('level', $this->levelFilter);
+        }
+
+        if(!is_null($this->roundFilter)){
+            $query->where('round', $this->roundFilter);
+        }
+
+        if(!is_null($this->characterFilter)){
+            $query->where('character_name', $this->characterFilter);
+        }
+
+        if(!is_null($this->searchText)){
+            $query->where('name', 'like', '%' . $this->searchText . '%');
+        }
+
+        $this->activeFilters = implode(' | ', $filters);
+        $this->cardResults = $query->get();
     }
 }
--- resources/views/livewire/card-search.blade.php
+++ resources/views/livewire/card-search.blade.php
@@ -1,15 +1,70 @@
-<div id="search-and-filter" class="grid grid-cols-1 gap-3">
-    <input type="text" wire:blur name="search_text" id="search_text" placeholder="Search for a card"/>
+<div>
+    <div id="search-and-filter" class="grid grid-cols-3 gap-3 p-2">
+        <input type="text" wire:model.blur="searchText" name="search_text" id="search_text" placeholder="Search for a card"/>
 
-    <select name="feature_filter" id="featureFilter">
-        @foreach($featureOptions as $feature)
-        <option value="{{ $feature }}">{{ ucfirst(str_replace('_', ' ', $feature)) }}</option>
-        @endforeach
-    </select>
 
-    <select name="type_filter" id="type_filter">
-        @foreach($typeOptions as $type)
-        <option value="{{ $type }}">{{ ucfirst($type) }}</option>
+        <select name="feature_filter" id="featureFilter" wire:model.change="featureFilter">
+            <option selected value="">-- Feature --</option>
+            @foreach($featureOptions as $feature)
+            <option value="{{ $feature }}">{{ ucfirst(str_replace('_', ' ', $feature)) }}</option>
+            @endforeach
+        </select>
+
+        <select name="rarity_filter" id="rarity_filter" wire:model.change="rarityFilter">
+            <option selected value="">-- Rarity --</option>
+            @foreach($rarityOptions as $rarity)
+            <option value="{{ $rarity }}">{{ ucfirst($rarity) }}</option>
+            @endforeach
+        </select>
+
+        <select name="character_filter" id="character_filter" wire:model.change="characterFilter">
+            <option selected value="">-- Character --</option>
+            @foreach($characterOptionsByGroup as $group => $characterOptions)
+            <optgroup label="{{ $group }}">
+                @foreach($characterOptions as $characterOption)
+                <option value="{{ $characterOption['character_name'] }}">{{ $characterOption['character_name']}}</option>
+                @endforeach
+            </optgroup>
+            @endforeach
+        </select>
+
+        
+        <select name="type_filter" id="type_filter" wire:model.change="typeFilter">
+            <option selected value="">-- Type --</option>
+            @foreach($typeOptions as $type)
+            <option value="{{ $type }}">{{ ucfirst($type) }}</option>
+            @endforeach
+        </select>
+
+        <select name="level_filter" id="level_filter" wire:model.change="levelFilter">
+            <option selected value="">-- Level --</option>
+            @foreach($levelOptions as $level)
+            <option value="{{ $level }}">Level {{ ucfirst($level) }}</option>
+            @endforeach
+        </select>
+
+
+        <select name="round_filter" id="round_filter" wire:model.change="roundFilter">
+            <option value="">-- Scene Round --</option>
+            @foreach($roundOptions as $round)
+            <option selected="{{ $round === $roundFilter ? 'selected' : ''}}" value="{{ $round }}">Round {{ $round }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div wire:text.live="activeFilters"></div>
+
+    <div id="card-results">
+        @if(empty($cardResults))
+        No cards found.
+        @else
+        <div class="flex-1 max-h-[calc(100vh-10rem)] overflow-y-scroll grid grid-cols-3 bg-green-200 gap-2 lg:gap-3 w-full">
+        @foreach($cardResults as $card)
+            <div class="flex w-34 lg:w-44 h-34 lg:h-44 bg-green-300 dark:bg-green-600" data-card-id="{{ $card->id }}">
+                <img src="{{ $card->thumbnail_url }}" alt="{{ $card->number }}" title="{{ $card->title }}" class="m-auto w-auto h-auto max-w-34 lg:max-w-44 max-h-34 lg:max-h-44"/>
+            </div>
         @endforeach
-    </select>
+        </div>
+        @endif
+    </div>
 </div>
